3c8313531d46e5df4d4f9860acaa0be1b01a3d49
SaNeK32 <andrejapecko@gmail.com>
2024-03-16 14:24:50 +0200

    Express

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello NodeJS!');
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+     
+    response.send("<h1>Главная страница</h1>");
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/about", (request, response) => {
+     
+    response.send("<h1>О сайте</h1>");
 });
+app.get("/contact", (request, response) => {
+     
+    response.send("<h1>Контакты</h1>");
+});
+app.listen(3000);
